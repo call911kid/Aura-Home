bfbd7ef52a526e84415a451cbe7c8f9b723172a1
Mustafa Hassan <91998433+call911kid@users.noreply.github.com>
2026-02-07 02:04:23 +0200

    Adding Homo Video & Fixing Newsettler

--- Scripts/HomeScript.js
+++ Scripts/HomeScript.js
@@ -69,6 +69,34 @@ document.getElementById("search-input").addEventListener("keydown", function(eve
 
 
 
+/*Fady's Newsettler*/
+
+let email = document.querySelector("#email");
+let subscribe = document.querySelector(".btn-subscribe");
+let message = document.querySelector(".message");
+let emailRegx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+subscribe.addEventListener("click", function () {
+  let emailValue = email.value.trim();
+  if (emailValue === "" || !emailRegx.test(emailValue)) {
+    message.textContent = "Invalid email! Please use a valid email";
+    message.style.visibility = "visible";
+    message.style.color = "#C96A5A";
+  } else {
+    message.textContent = "Success! You are now subscribed";
+    message.style.visibility = "visible";
+    message.style.color = "#4F8A7A";
+    email.value = "";
+  }
+});
+
+
+document.getElementById("email").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        subscribe.click();
+    }
+});
+
+
 
 
 
